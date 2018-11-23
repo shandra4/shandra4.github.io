b75b084450fcef711a03d60382a54d27297e9bfd
julienr2 <julien.rougeron@gmail.com>
2018-11-23 09:17:36 +0100

    🌱

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Shandra's Portfolio`,
+        short_name: `Shandra's Portfolio`,
+        start_url: '/',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        precision: 8,
+      },
+    },
+  ],
+}
